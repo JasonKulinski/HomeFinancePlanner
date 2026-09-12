b60d5013a01f1cf55bb9ac0024dee2d468799f7a
Jason Kulinski <jason.kulinski@maine.edu>
2026-09-12 19:02:27 -0400

    Middle tier API request to backend calculator logic

---
+++ src/HomeFinancePlanner.Models/DTOs/UserHomeRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace HomeFinancePlanner.Models.DTOs;
+
+public class UserHomeRequest
+{
+    public int homeId { get; set; }
+    public int userId { get; set; }
+}
---
+++ src/HomeFinancePlanner.Models/DTOs/UserHomeResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace HomeFinancePlanner.Models.DTOs;
+
+public class UserHomeResponse
+{
+    public int timeToDownPaymentInMonths { get; set; }
+    public int timeToPurchasedInMonths { get; set; }
+    public int estimatedMonthlyPayment { get; set; }
+
+}
