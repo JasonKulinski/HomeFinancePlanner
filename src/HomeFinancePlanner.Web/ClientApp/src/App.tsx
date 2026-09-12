48a5ad6e27c5ebf8e93649cb26aa37a40b6ab801
Lucas <lucasowenbent@gmail.com>
2026-09-12 16:22:50 -0400

    map display

--- src/HomeFinancePlanner.Web/ClientApp/src/App.tsx
+++ src/HomeFinancePlanner.Web/ClientApp/src/App.tsx
@@ -1,7 +1,6 @@
 import AffordabilityCalculator from './components/AffordabilityCalculator'
-import { Cartesian3, Ion, IonGeocodeProviderType } from 'cesium'
-import { useEffect } from 'react'
-import { Entity, EntityDescription, GooglePhotorealistic3DTileset, PointGraphics, Viewer } from 'resium'
+import { Cartesian3, Ion } from 'cesium'
+import { Entity, EntityDescription, PointGraphics, Viewer } from 'resium'
 
 Ion.defaultAccessToken =
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub25jZSI6IllkdFFDMkFiOW45TXZPb0siLCJqdGkiOiI2ZDVjNmExZC1iNGZiLTRlZDgtYjJiZi0xMjZjN2JmZTJiMGIiLCJpZCI6NDkxMzQxLCJpc3MiOiJodHRwczovL2FwaS5jZXNpdW0uY29tIiwiYXVkIjoidW5kZWZpbmVkX2RlZmF1bHQiLCJpYXQiOjE3ODkyMzk4MzZ9._0SlTFQOOIYzm_IdBuqunFbrqu4Xulbad2cIimyIhvU'
@@ -9,14 +8,6 @@ const position = Cartesian3.fromDegrees(-74.0707383, 40.7117244, 100)
 const pointGraphics = { pixelSize: 100 }
 
 export default function App() {
-    // async function test() {
-    //     const tileset = await Cesium.Cesium3DTileset.fromIonAssetId(2275207)
-    // }
-
-    // useEffect(() => {
-    //     test
-    // }, [])
-
     return (
         <main className='page'>
             <header className='page-header'>
@@ -25,7 +16,7 @@ export default function App() {
             </header>
             <AffordabilityCalculator />
             <Viewer
-                full
+                // full
                 timeline={false}
                 navigationHelpButton={false}
                 // geocoder={IonGeocodeProviderType.GOOGLE}
